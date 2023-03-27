e5ee96f85746d584df77e96525c587a353259e39
Surya152002 <108388181+Surya152002@users.noreply.github.com>
2023-03-27 11:48:37 +0530

    Update file.py

--- file.py
+++ file.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
-# set up the Tesseract OCR engine
+# Set up Tesseract OCR engine
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 @app.route('/')
@@ -12,23 +12,23 @@ def index():
     return render_template('index.html')
 
 def gen():
-    # initialize the video capture device
+    # Initialize video capture device for back camera
     cap = cv2.VideoCapture(0)
 
     while True:
-        # read the frame from the camera
+        # Read frame from camera
         ret, frame = cap.read()
 
-        # apply some image preprocessing to improve the OCR accuracy
+        # Preprocess image to improve OCR accuracy
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (5, 5), 0)
         gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-        # detect the number plate in the frame using OpenCV
+        # Detect number plate using OpenCV
         plate_cascade = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
         plates = plate_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-        # if a number plate is detected, extract the text using Tesseract OCR
+        # If number plate detected, extract text using Tesseract OCR
         if len(plates) > 0:
             for (x, y, w, h) in plates:
                 plate_img = gray[y:y+h, x:x+w]
@@ -36,7 +36,7 @@ def gen():
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
                 cv2.putText(frame, plate_text, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-        # convert the frame to a JPEG image
+        # Convert frame to JPEG image
         ret, jpeg = cv2.imencode('.jpg', frame)
         frame = jpeg.tobytes()
 
@@ -45,7 +45,8 @@ def gen():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
